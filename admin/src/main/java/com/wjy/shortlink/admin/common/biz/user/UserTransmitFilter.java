f1c0022bc30d8c8709e44666eff502b6a4bcf5ce
wjy <1750866610@qq.com>
2024-08-15 12:16:44 +0800

    过滤器处理异常完善

--- admin/src/main/java/com/wjy/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/wjy/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,28 +1,63 @@
 package com.wjy.shortlink.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
+import com.wjy.shortlink.admin.common.convention.exception.ClientException;
+import com.wjy.shortlink.admin.common.convention.result.Result;
+import com.wjy.shortlink.admin.common.convention.result.Results;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 import java.util.Objects;
+
+import static com.wjy.shortlink.admin.common.enums.UserErrorCode.USER_TOKEN_FAIL;
 
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate stringRedisTemplate;
+    private static final List<String> ignore_URI = Lists.newArrayList(
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/v1/user/has-username"
+
+    );
+    @SneakyThrows
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
-        if(!Objects.equals(requestURI,"/api/short-link/v1/user/login")){
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            Object userInfo = stringRedisTemplate.opsForHash().get("login_" + username, token);
-            if(userInfo!=null){
-                UserInfoDTO userInfoDTO = JSON.parseObject(userInfo.toString(), UserInfoDTO.class);
-                UserContext.setUser(userInfoDTO);
+        if(!ignore_URI.contains(requestURI)){
+            String method = httpServletRequest.getMethod();
+            if(!(Objects.equals(requestURI,"/api/short-link/admin/v1/user")&&Objects.equals(method,"POST"))){
+                    String username = httpServletRequest.getHeader("username");
+                    String token = httpServletRequest.getHeader("token");
+                    if(!StrUtil.isAllNotBlank(username,token)){
+                        getOutException((HttpServletResponse) servletResponse, Results.failure(new ClientException(USER_TOKEN_FAIL)));
+                        return;
+                    }
+                    Object userInfo = null;
+                    try {
+                        userInfo = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                        if(userInfo==null){
+                            getOutException((HttpServletResponse) servletResponse, Results.failure(new ClientException(USER_TOKEN_FAIL)));
+                            return;
+                        }
+                    }catch (Exception ex){
+                        getOutException((HttpServletResponse) servletResponse, Results.failure(new ClientException(USER_TOKEN_FAIL)));
+                        return;
+                    }
+                    if(userInfo!=null){
+                        UserInfoDTO userInfoDTO = JSON.parseObject(userInfo.toString(), UserInfoDTO.class);
+                        UserContext.setUser(userInfoDTO);
+                    }
+
             }
         }
 
@@ -32,4 +67,14 @@ public class UserTransmitFilter implements Filter {
             UserContext.removeUser();
         }
         }
+
+    private void getOutException(HttpServletResponse response, Result r) throws Exception {
+        response.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String jsonObject = JSON.toJSONString(r);
+        out.println(jsonObject);
+        out.flush();
+        out.close();
+    }
+
 }
